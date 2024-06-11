a5b957dafec7298de1cef7eb421b31c7ba6eb76d
Ramesh Kumar Guntamukkala <rameshkumar.gu@gmail.com>
2024-06-11 19:31:57 +0530

    Add SpringBoot Project

---
+++ src/main/java/com/home/expenses/ExpensesTrackerApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.home.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTrackerApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesTrackerApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/expenses/ExpensesTrackerApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackerApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
